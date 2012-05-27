62a78fdb8df25c0a5ee66a4bb0b1a2413d9ece68
feng <feng.shen@onycloud.com>
2012-05-27 22:59:30 +0800

    change wait_timeout to 3 days if is default 8 hours

--- src/main/java/me/shenfeng/dbcp/ThreadLocalConnection.java
+++ src/main/java/me/shenfeng/dbcp/ThreadLocalConnection.java
@@ -2,7 +2,9 @@ package me.shenfeng.dbcp;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -11,60 +13,73 @@ import org.slf4j.LoggerFactory;
 
 public class ThreadLocalConnection extends ThreadLocal<Connection> {
 
-	private final String url;
-	private final String username;
-	private final String password;
+    private final String url;
+    private final String username;
+    private final String password;
 
-	private final static Logger logger = LoggerFactory
-			.getLogger(ThreadLocalConnection.class);
+    private final static Logger logger = LoggerFactory
+            .getLogger(ThreadLocalConnection.class);
 
-	private ConcurrentLinkedQueue<Connection> cons = new ConcurrentLinkedQueue<Connection>();
-	private AtomicInteger id = new AtomicInteger(0);
+    private ConcurrentLinkedQueue<Connection> cons = new ConcurrentLinkedQueue<Connection>();
+    private AtomicInteger id = new AtomicInteger(0);
 
-	public ThreadLocalConnection(String url, String username, String password) {
-		this.url = url;
-		this.username = username;
-		this.password = password;
-	}
+    public ThreadLocalConnection(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
 
-	public void close() {
-		logger.info("close all connections");
-		Connection con = null;
-		while ((con = cons.poll()) != null) {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				logger.error("close all connections", e);
-			}
-		}
-	}
+    public void close() {
+        logger.info("close all connections");
+        Connection con = null;
+        while ((con = cons.poll()) != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                logger.error("close all connections", e);
+            }
+        }
+    }
 
-	public Connection get() {
-		Connection con = super.get();
-		try {
-			if (con.isClosed()) {
-				logger.trace("a connection is closed, try to create a new one");
-				remove();
-				return super.get(); // try to create a new one
-			}
-		} catch (SQLException e) {
-			logger.error("error when asking isClosed!", e);
-		}
-		return con;
-	}
+    public Connection get() {
+        Connection con = super.get();
+        try {
+            if (con.isClosed()) {
+                logger.trace("a connection is closed, try to create a new one");
+                remove();
+                return super.get(); // try to create a new one
+            }
+        } catch (SQLException e) {
+            logger.error("error when asking isClosed!", e);
+        }
+        return con;
+    }
 
-	protected Connection initialValue() {
-		try {
-			int i = id.incrementAndGet();
-			logger.trace("create connection " + i);
-			Connection con = DriverManager.getConnection(url, username,
-					password);
-			cons.add(con);
-			return new NoCloseConnection(con);
-		} catch (SQLException e) {
-			logger.error("create connection", e);
-			throw new RuntimeException(e);
-		}
-	}
+    protected Connection initialValue() {
+        try {
+            int i = id.incrementAndGet();
+            logger.trace("create connection " + i);
+            Connection con = DriverManager.getConnection(url, username,
+                    password);
+            Statement stat = con.createStatement();
+            ResultSet rs = stat
+                    .executeQuery("show variables like 'wait_timeout'");
+            if (rs.next()) {
+                int timeout = rs.getInt(2);
+                if (timeout == 3600 * 8) {
+                    logger.debug("change default wait_timeout from 8 hours to 3 days");
+                    stat.executeUpdate("set wait_timeout = 259200");
+                }
+            }
+            rs.close();
+            stat.close();
+
+            cons.add(con);
+            return new NoCloseConnection(con);
+        } catch (SQLException e) {
+            logger.error("create connection", e);
+            throw new RuntimeException(e);
+        }
+    }
 
 }
---
+++ src/test/java/me/shenfeng/dbcp/TimeoutTest.java
@@ -0,0 +1,69 @@
+package me.shenfeng.dbcp;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Test;
+
+public class TimeoutTest {
+
+    private void printResultSet(ResultSet rs) throws SQLException {
+        ResultSetMetaData metaData = rs.getMetaData();
+
+        int count = metaData.getColumnCount();
+        for (int i = 1; i <= count; i++) {
+            String latel = metaData.getColumnLabel(i);
+            System.out.printf(latel + "\t");
+        }
+        System.out.println();
+
+        while (rs.next()) {
+            for (int i = 1; i <= count; i++) {
+
+                Object value = rs.getObject(i);
+                System.out.printf(value + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    PerThreadDataSource dataSource = new PerThreadDataSource(
+            "jdbc:mysql://localhost/rssminer?maintainTimeStats=false",
+            "feng", "");
+
+    @Test
+    public void testTimeout() throws SQLException, InterruptedException {
+        Connection con = dataSource.getConnection();
+
+        Statement statment = con.createStatement();
+        ResultSet rs = statment
+                .executeQuery("show global variables like 'wait_timeout'");
+        printResultSet(rs);
+        rs.close();
+
+        rs = statment.executeQuery("show variables like 'wait_timeout'");
+        printResultSet(rs);
+        rs.close();
+
+        statment.executeUpdate("set wait_timeout = 2");
+
+        rs = statment.executeQuery("show variables like 'wait_timeout'");
+        printResultSet(rs);
+        rs.close();
+
+        Thread.sleep(3000);
+        // should timeout
+
+        try {
+            rs = statment.executeQuery("show variables like 'wait_timeout'");
+            printResultSet(rs);
+            rs.close();
+        } catch (SQLException ignore) {
+            ignore.printStackTrace();
+        }
+
+    }
+}
