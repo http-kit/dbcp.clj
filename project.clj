c19d00d11ea174a892e91b223e3337430000f040
feng <feng.shen@onycloud.com>
2013-02-13 16:35:49 +0800

    update project.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject http-kit/dbcp "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "High performance database connection pool for Clojure"
+  :url "https://github.com/http-kit/dbcp.clj"
   :warn-on-reflection true
   :java-source-path "src/java"
   :jar-exclusions [#".*java$"]
   :javac-options {:source "1.6" :target "1.6" :debug "true" :fork "true"}
   :plugins [[lein-swank "1.4.4"]]
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.2.3"]]
   :profiles {:dev {:dependencies [[mysql/mysql-connector-java "5.1.21"]
